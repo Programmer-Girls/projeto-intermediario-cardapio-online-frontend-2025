fbe68c9bc6d3325d458039cf1fac4a3ac02538be
Karin Abe <karin.abe@gmail.com>
2025-04-22 17:52:54 -0300

    atualizando repositório

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "./App.scss";
+import "./Styles/App.scss";
+import Header from "./Components/Header";
+import Acompanhamentos from "./Components/Acompanhamentos";
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Acompanhamentos />
     </div>
   );
 }
